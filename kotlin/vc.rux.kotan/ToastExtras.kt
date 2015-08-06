a0d3413ef126370d08b107330c400313e85a2596
ruX [Ruslan Zaharov] <post4ruX@gmail.com>
2015-08-06 15:39:49 +0100

    Toast extention now support text as resource

--- kotlin/vc.rux.kotan/ToastExtras.kt
+++ kotlin/vc.rux.kotan/ToastExtras.kt
@@ -11,13 +11,23 @@ inline fun toast(context: Context, text: CharSequence, length: Int) {
 fun toastLong(context: Context, text: CharSequence) =
     toast(context, text, Toast.LENGTH_LONG)
 
-
 fun toastShort(context: Context, text: CharSequence) =
     toast(context, text, Toast.LENGTH_SHORT)
 
+fun toastLong(context: Context, textResourceId: Int) =
+    toast(context, context.getString(textResourceId), Toast.LENGTH_LONG)
+
+fun toastShort(context: Context, textResourceId: Int) =
+    toast(context, context.getString(textResourceId), Toast.LENGTH_SHORT)
 
 fun Activity.toastLong(text: CharSequence) =
     toastLong(this, text)
 
 fun Activity.toastShort(text: CharSequence) =
     toastShort(this, text)
+
+fun Activity.toastLong(textResourceId: Int) =
+    toastLong(this, textResourceId)
+
+fun Activity.toastShort(textResourceId: Int) =
+    toastShort(this, textResourceId)
