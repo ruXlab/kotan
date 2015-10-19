25c4ecc38640e9a8d4fa659d99a0d61d9397fd3a
ruX [Ruslan Zaharov] <post4ruX@gmail.com>
2015-10-19 17:40:47 +0100

    Cursor to entity mapper

--- kotlin/vc.rux.kotan/CursorExtras.kt
+++ kotlin/vc.rux.kotan/CursorExtras.kt
@@ -1,6 +1,7 @@
 package vc.rux.kotan
 
 import android.database.Cursor
+import java.util.*
 
 
 fun Cursor.getString(columnName: String) =
@@ -23,3 +24,22 @@ fun Cursor.getShort(columnName: String) =
 
 fun Cursor.getBlob(columnName: String) =
     getBlob(getColumnIndexOrThrow(columnName))
+
+/**
+ * Pass remaining cursor entities via mapper function
+ */
+fun Cursor.mapFromCurrent<T>(f: (record: Cursor) -> T): List<T> {
+    val result = ArrayList<T>()
+    do {
+        result.add(f(this))
+    } while (moveToNext())
+    return result
+}
+
+/**
+ * Resets cursor to the start and map each entry
+ */
+fun Cursor.mapAll<T>(f: (record: Cursor) -> T): List<T> =
+    if (moveToFirst()) mapFromCurrent(f)
+    else ArrayList<T>(0)
+
