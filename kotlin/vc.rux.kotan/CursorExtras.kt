4334558b282ffed7009be6ed7e7c1bc3e319bc89
ruX [Ruslan Zaharov] <post4ruX@gmail.com>
2015-10-21 18:28:00 +0100

    Overloaded methods for Cursor.get* with default value

--- kotlin/vc.rux.kotan/CursorExtras.kt
+++ kotlin/vc.rux.kotan/CursorExtras.kt
@@ -7,23 +7,44 @@ import java.util.*
 fun Cursor.getString(columnName: String) =
     getString(getColumnIndexOrThrow(columnName))
 
+fun Cursor.getString(columnName: String, default: String?) =
+    try { getString(columnName) } catch(e: Exception) { default }
+
 fun Cursor.getInt(columnName: String) =
     getInt(getColumnIndexOrThrow(columnName))
+
+fun Cursor.getInt(columnName: String, default: Int?) =
+    try { getInt(columnName) } catch(e: Exception) { default }
 
 fun Cursor.getDouble(columnName: String) =
     getDouble(getColumnIndexOrThrow(columnName))
 
+fun Cursor.getDouble(columnName: String, default: Double?) =
+    try { getInt(columnName) } catch(e: Exception) { default }
+
 fun Cursor.getFloat(columnName: String) =
     getFloat(getColumnIndexOrThrow(columnName))
+
+fun Cursor.getFloat(columnName: String, default: Float?) =
+    try { getFloat(columnName) } catch(e: Exception) { default }
 
 fun Cursor.getLong(columnName: String) =
     getLong(getColumnIndexOrThrow(columnName))
 
+fun Cursor.getLong(columnName: String, default: Long?) =
+    try { getLong(columnName) } catch(e: Exception) { default }
+
 fun Cursor.getShort(columnName: String) =
     getShort(getColumnIndexOrThrow(columnName))
 
+fun Cursor.getShort(columnName: String, default: Short?) =
+    try { getShort(columnName) } catch(e: Exception) { default }
+
 fun Cursor.getBlob(columnName: String) =
     getBlob(getColumnIndexOrThrow(columnName))
+
+fun Cursor.getShort(columnName: String, default: ByteArray?) =
+    try { getBlob(columnName) } catch(e: Exception) { default }
 
 /**
  * Pass remaining cursor entities via mapper function
